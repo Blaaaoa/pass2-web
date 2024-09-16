257c3a9cf8cb9f6f489d857ddfc30f4e69031907
AugustoV2 <augustovarghese@gmail.com>
2024-09-17 01:39:03 +0530

    endhoo ayyi

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,24 +1,24 @@
-// utils/api.js
-
-export async function runCode(inputFileContent: string) {
+export async function runCode(inputFileContent: any) {
     try {
-      const response = await fetch('http://127.0.0.1:5000/', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ input_file_content: inputFileContent }),
-      });
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      const result = await response.json();
-      return result;
+        const response = await fetch('http://127.0.0.1:5000/run-code', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                code: inputFileContent
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const result = await response.json();
+        console.log('Response from server:', result); 
+        return result;
     } catch (error) {
-      console.error('Error:', error);
-      return { error: (error as any).message };
+        console.error('Error:', error);
+        return { error: (error as Error).message };
     }
-  }
-  
+}
